fba26d942510804dc3cac6261467313b0741daed
kixelated <kixelated@gmail.com>
2025-04-17 16:51:21 -0700

    Add a tracing feature. (#22)
    Just does .in_current_span() because it's annoying to do it manually.

--- web-async/src/spawn.rs
+++ web-async/src/spawn.rs
@@ -7,10 +7,14 @@ use std::future::Future;
 
 #[cfg(any(not(target_arch = "wasm32"), target_os = "wasi"))]
 pub fn spawn<F: Future<Output = ()> + Send + 'static>(f: F) {
+	#[cfg(feature = "tracing")]
+	let f = tracing::Instrument::in_current_span(f);
 	tokio::task::spawn(f);
 }
 
 #[cfg(all(target_arch = "wasm32", not(target_os = "wasi")))]
 pub fn spawn<F: Future<Output = ()> + 'static>(f: F) {
+	#[cfg(feature = "tracing")]
+	let f = tracing::Instrument::in_current_span(f);
 	wasm_bindgen_futures::spawn_local(f);
 }
