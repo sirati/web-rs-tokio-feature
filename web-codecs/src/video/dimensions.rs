039dae8ba8b469da52d5bdfad94d45a1d0986911
kixelated <kixelated@gmail.com>
2025-05-19 09:52:27 -0700

    Add PartialEq/Eq to Dimensions (#26)

--- web-codecs/src/video/dimensions.rs
+++ web-codecs/src/video/dimensions.rs
@@ -1,5 +1,11 @@
-#[derive(Debug, Clone, Copy, Default)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
 pub struct Dimensions {
 	pub width: u32,
 	pub height: u32,
+}
+
+impl Dimensions {
+	pub fn new(width: u32, height: u32) -> Self {
+		Self { width, height }
+	}
 }
